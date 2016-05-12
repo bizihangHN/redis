b7da56ee92f7a31b8d65412ecf4e343d2f8d95bd
bizihang <951755883@qq.com>
2016-05-12 11:46:42 +0800

    init

---
+++ src/main/java/com/bizihang/Main.java
@@ -0,0 +1,9 @@
+package com.bizihang;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		Arrays.asList("Hello", " world", "!").forEach(e -> System.out.print(e));
+	}
+}
